f5a43da6e7691e82aebb9e8f836e5a7e1e263469
kidus-t-w <kidusk69@gmail.com>
2024-06-20 15:09:30 +0300

    Getting started...

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, TypeScript!");
+  res.send("Hello, TypeScript From Express!");
 });
 
 export default app;
