7c49ca8e9de0279a2dfd389be6033afabea571e4
kidus-t-w <kidusk69@gmail.com>
2024-06-25 20:35:12 +0300

    added routes for pages

--- src/client/src/main.tsx
+++ src/client/src/main.tsx
@@ -5,12 +5,17 @@ import "./index.css";
 import Home from "./pages/Home.tsx";
 import Profile from "./pages/Profile.tsx"
 import Services from "./pages/Services.tsx"
+import PropertyDetail from '@/pages/PropertyDetail.tsx'
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
 } from "react-router-dom";
+import PropertyListing from "./pages/PropertyListing.tsx";
+import CreateListing from "./pages/CreateListing.tsx";
+import Contact from "./pages/Contact.tsx";
+import Login from "./pages/Login.tsx";
 
 const router = createBrowserRouter([
   {
@@ -28,9 +33,29 @@ const router = createBrowserRouter([
       {
         path: 'services',
         element: <Services/>
+      },
+      {
+        path: 'property_listing',
+        element: <PropertyListing/>
+      },
+      {
+        path: 'property_detail',
+        element: <PropertyDetail />
+      },
+      {
+        path: 'create_listing',
+        element: <CreateListing />
+      },
+      {
+        path: 'contact',
+        element: <Contact />
       }
     ]
   },
+  {
+    path: "/login",
+    element: <Login/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
