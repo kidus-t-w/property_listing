afe81fc655f016742470b933161d60c6ae870191
kidus-t-w <kidusk69@gmail.com>
2024-06-25 20:35:46 +0300

    Setup app.tsx

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import Header from "./components/common/Header";
+import { Footer } from "./components/common/Footer";
 
 export default function App() {
 
@@ -10,6 +11,7 @@ export default function App() {
       <Header />
       <div className="m"></div>
       <Outlet/>
+      <Footer/>
     </body>
   );
 }
