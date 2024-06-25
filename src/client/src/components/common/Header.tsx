b99af8b1173ac68bf6632a1bda79a666069e5a4a
kidus-t-w <kidusk69@gmail.com>
2024-06-25 20:40:26 +0300

    Modified header design and linked pages to header

--- src/client/src/components/common/Header.tsx
+++ src/client/src/components/common/Header.tsx
@@ -12,15 +12,16 @@ export default function Header() {
   return (
     <header className="flex flex-row items-center justify-between sm:justify-around p-2 border-b-2 bg-gray-100 mb-4">
       <Link to="/" className="flex items-center">
-      </Link>
         <img src={Logo} alt="RealEthio Logo" className="h-10  sm:block" />
+      </Link>
       <Navbar />
       <div className="flex items-center space-x-4">
-        <Button variant="destructive" className="hidden sm:block">Free Listing</Button>
+        
+        <Button variant="destructive" className="hidden sm:block">< Link to='/create_listing'>Free Listing</Link></Button>
         <Link to="/login" className="hidden sm:block text-gray-700 hover:text-gray-900">
           Login
         </Link>
-        <Link to="/signup" className="hidden sm:block text-gray-700 hover:text-gray-900">
+        <Link to="/login" className="hidden sm:block text-gray-700 hover:text-gray-900">
           Signup
         </Link>
         {/* <Button className="md:hidden"><Menu /></Button> */}
