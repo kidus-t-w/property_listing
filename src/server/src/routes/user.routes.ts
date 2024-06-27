bdb78efb8d307e9082559573018ed94d7f3453fb
kidus-t-w <kidusk69@gmail.com>
2024-06-27 14:49:10 +0300

    Created endpoints for user

--- src/server/src/routes/user.routes.ts
+++ src/server/src/routes/user.routes.ts
@@ -1,8 +1,26 @@
 import { Router } from "express";
-import {getUsersHandler} from '../controllers/users.controller'
+import {
+  getUsersHandler,
+  createUserHandler,
+  updateUserHandler,
+  deleteUserHandler,
+  getAuthenticatedUserHandler,
+  getSingleUserHandler,
+} from "../controllers/users.controller";
+import validateResource from "../utils/validateResource";
+import { createUserSchema } from "../schemas/user.schema";
 
 const userRouter = Router();
 
-userRouter.get('/', getUsersHandler)
+userRouter.get("/", getUsersHandler);
+// userRouter.get("/me", getAuthenticatedUserHandler);
+
+/**
+ * Creates a new user
+ */
+userRouter.post("/", validateResource(createUserSchema), createUserHandler);
+
+userRouter.put("/", updateUserHandler);
+userRouter.delete("/:id", deleteUserHandler);
 
 export default userRouter;
