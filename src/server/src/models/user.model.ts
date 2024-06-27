de4fdee4a89d9a53ea648f8ebb8b208be25f884e
kidus-t-w <kidusk69@gmail.com>
2024-06-27 14:50:24 +0300

    Modified UserModel

--- src/server/src/models/user.model.ts
+++ src/server/src/models/user.model.ts
@@ -1,29 +1,31 @@
 import mongoose from "mongoose";
+import { nullable } from "zod";
 
 export interface UserInput {
-    firstName: string;
-    lastName: string;
-    password: string;
-    email: string;
-    phoneNumber: string;
+  firstName: string | null;
+  lastName: string | null;
+  password: string;
+  email: string;
+  phoneNumber: string | null;
 }
 
 export interface UserDocumnet extends UserInput, mongoose.Document {
-    createdAt: Date;
-    updatedAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const userSchema = new mongoose.Schema({
-    firstName: {type: String, require: true},
-    lastName: {type: String, require: true},
-    email: {type: String, require: true, unique: true},
-    phoneNumber: {type: String, require: true},
-    password: {type: String, require: true},
-},
-{
-    timestamps: true
-}
-)
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, require: false, nullable: true },
+    lastName: { type: String, require: false, nullable: true },
+    email: { type: String, require: true, unique: true },
+    phoneNumber: { type: String, require: false, nullable: true },
+    password: { type: String, require: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const UserModel = mongoose.model<UserDocumnet>("Users", userSchema)
+const UserModel = mongoose.model<UserDocumnet>("Users", userSchema);
 export default UserModel;
