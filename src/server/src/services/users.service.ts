d3043c3a4466a05796bb0ea65cb279fc687b2f98
kidus-t-w <kidusk69@gmail.com>
2024-06-27 14:51:26 +0300

    added addUser handler

--- src/server/src/services/users.service.ts
+++ src/server/src/services/users.service.ts
@@ -1,6 +1,21 @@
-import UserModel from "../models/user.model";
+import UserModel, { UserInput } from "../models/user.model";
 
-export async function getAllUsers(){
-    const users = await UserModel.find({})
-    return users
+export async function getAllUsers() {
+  const users = await UserModel.find({});
+  return users;
+}
+
+export async function createUser(values: UserInput) {
+  // I want to create a user
+  try {
+    const user = await UserModel.create(values);
+    return user;
+  } catch (err: any) {
+    return new Error("Email duplicate not allowed.");
+  }
+}
+
+export async function deleteUser(value: UserInput) {
+  const user = UserModel.deleteOne(value)
+  return user
 }
