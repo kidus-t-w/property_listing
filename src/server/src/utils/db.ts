ad9ff352b79d87032be6910c70afb8b511797fc1
kidus-t-w <kidusk69@gmail.com>
2024-06-27 14:48:21 +0300

    Connected database to mongodb atalas

--- src/server/src/utils/db.ts
+++ src/server/src/utils/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const url = 'mongodb://localhost:27017/product_listing'
-const deploymentUrl = 'mongodb+srv://kidusk:BXWF8llRLdzkwB5o@notdetuts.agr7oi9.mongodb.net/?retryWrites=true&w=majority&appName=notdetuts'
+const deploymentUrl = 'mongodb+srv://kidusk:BXWF8llRLdzkwB5o@notdetuts.agr7oi9.mongodb.net/product_listing?retryWrites=true&w=majority&appName=notdetuts'
 
 async function connect() {
     try {
