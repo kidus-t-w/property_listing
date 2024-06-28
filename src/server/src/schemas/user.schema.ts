e9acd31704a3e905c896ec3dcf80a2c5a6e03bde
kidus-t-w <kidusk69@gmail.com>
2024-06-28 13:00:11 +0300

    Added updateUserSchema schema for update validation

--- src/server/src/schemas/user.schema.ts
+++ src/server/src/schemas/user.schema.ts
@@ -16,4 +16,23 @@ export const createUserSchema = z.object({
   }),
 });
 
+export const updateUserSchema = z.object({
+  body: z.object({
+    firstName: z.string({ message: "First Name must be a string." }).optional(),
+    lastName: z.string({ message: "Last Name must be a string." }).optional(),
+    email: z
+      .string({ message: "Must be a valid email." })
+      .email({ message: "Must be a valid email." })
+      .optional(),
+    password: z
+      .string({ message: "Must be at least 6 characters." })
+      .min(6, { message: "Must be at least 6 characters." })
+      .optional(),
+    phoneNumber: z
+      .string({ message: "Phone Number must be a string." })
+      .optional(),
+  }),
+});
+
 export type CreateUserInput = z.infer<typeof createUserSchema>;
+export type UpdateUserInput = z.infer<typeof updateUserSchema>;
